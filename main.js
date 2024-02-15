bee4b42f70efefc957182b81f4fabfeb8183d576
Tânia <106488757+meganPopy@users.noreply.github.com>
2024-02-14 23:30:09 -0300

    aaa

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+console.log(botoes)
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
@@ -9,3 +10,4 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 }
+
